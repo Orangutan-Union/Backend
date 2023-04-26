e63eb028e5458e901699a0fae0e4a06edbd3ed23
jwn <jwn@PROELE10>
2023-04-26 13:52:48 +0200

    added databasecontext and connectionstring to db

---
+++ TECHUB.Repository/Context/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TECHUB.Repository.Models;
+
+namespace TECHUB.Repository.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+
+        }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=192.168.20.33,1433; Initial Catalog=TestDB; TrustServerCertificate=True; User ID=sa; Password=Passw0rd;");
+        }
+    }
+}
--- TECHUB.Repository/Program.cs
+++ TECHUB.Repository/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using TECHUB.Repository.Context;
+
 Console.WriteLine("Hello, World!");
+
+var services = new ServiceCollection();
+
+services.AddDbContext<DatabaseContext>();
