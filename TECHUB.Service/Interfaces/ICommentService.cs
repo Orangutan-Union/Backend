4769dc3d9027922021c5c2888b6f8b8e346813ca
Sebastian Munch <bassemunch@gmail.com>
2023-06-01 08:46:35 +0200

    Added GetCommentById to service

--- TECHUB.Service/Interfaces/ICommentService.cs
+++ TECHUB.Service/Interfaces/ICommentService.cs
@@ -4,6 +4,7 @@ namespace TECHUB.Service.Interfaces
 {
     public interface ICommentService
     {
+        Task<Comment> GetCommentById(int id);
         Task<Comment> CreateComment(Comment comment);
         Task<Comment> UpdateComment(Comment comment);
         Task<Comment> DeleteComment(int id);
