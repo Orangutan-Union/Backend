d02fb9088645e269c3d99ff891d88c8a1939657e
Sebastian Munch <bassemunch@gmail.com>
2023-06-02 12:09:03 +0200

    post feed

--- TECHUB/Controllers/PostController.cs
+++ TECHUB/Controllers/PostController.cs
@@ -162,7 +162,7 @@ namespace TECHUB.API.Controllers
         [HttpGet("feed/{id:int}")]
         public async Task<IActionResult> GetUserFeed(int id)
         {
-            var friendFollowers = await context.FriendFollowers
+            var test = await context.FriendFollowers
                 .Where(ff => ff.Type != 3 && ff.UserId == id || ff.Type != 3 && ff.OtherUserId == id)
                 .Select(ff => new
                 {
@@ -171,153 +171,78 @@ namespace TECHUB.API.Controllers
                     User = new
                     {
                         UserId = ff.User.UserId,
-                        Posts = ff.User.Posts.Select(p => new
-                        {
-                            PostId = p.PostId,
-                            UserId = p.UserId,
-                            GroupId = p.GroupId,
-                            TimeStamp = p.TimeStamp,
-                            Content = p.Content,
-                            FriendOnly = p.FriendOnly,
-                            Latitude = p.Latitude,
-                            Longitude = p.Longitude,
-                            Group = new
-                            {
-                                GroupId = p.Group.GroupId,
-                                GroupName = p.Group.GroupName,
-                                Picture = new
-                                {
-                                    PictureId = p.Group.Picture.PictureId,
-                                    ImageUrl = p.Group.Picture.ImageUrl,
-                                }
-                            },
-                            User = new
-                            {
-                                UserId = p.User.UserId,
-                                DisplayName = p.User.DisplayName,
-                                Picture = new
-                                {
-                                    PictureId = p.User.Picture.PictureId,
-                                    ImageUrl = p.User.Picture.ImageUrl,
-                                }
-                            },
-                            Likes = p.Likes.Select(pl => new
-                            {
-                                UserId = pl.UserId,
-                                PostId = pl.PostId,
-                                IsLiked = pl.IsLiked,
-                                IsDisliked = pl.IsDisliked,
-                                User = new
-                                {
-                                    UserId = pl.User.UserId,
-                                    DisplayName = pl.User.DisplayName,
-                                }
-                            }).ToList(),
-                            Comments = p.Comments.Select(c => new
-                            {
-                                CommentId = c.CommentId,
-                                UserId = c.UserId,
-                                TimeStamp = c.TimeStamp,
-                                Content = c.Content,
-                                Likes = c.Likes.Select(cl => new
-                                {
-                                    IsLiked = cl.IsLiked,
-                                    IsDisliked = cl.IsDisliked,
-                                    User = new
-                                    {
-                                        UserId = cl.User.UserId,
-                                        DisplayName = cl.User.DisplayName,
-                                    }
-                                }).ToList(),
-                            }).ToList(),
-                        }).ToList()
                     },
                     OtherUser = new
                     {
                         UserId = ff.OtherUser.UserId,
-                        Posts = ff.OtherUser.Posts.Select(p => new
-                        {
-                            PostId = p.PostId,
-                            UserId = p.UserId,
-                            GroupId = p.GroupId,
-                            TimeStamp = p.TimeStamp,
-                            Content = p.Content,
-                            FriendOnly = p.FriendOnly,
-                            Latitude = p.Latitude,
-                            Longitude = p.Longitude,
-                            Group = new
-                            {
-                                GroupId = p.Group.GroupId,
-                                GroupName = p.Group.GroupName,
-                                Picture = new
-                                {
-                                    PictureId = p.Group.Picture.PictureId,
-                                    ImageUrl = p.Group.Picture.ImageUrl,
-                                }
-                            },
-                            User = new
-                            {
-                                UserId = p.User.UserId,
-                                DisplayName = p.User.DisplayName,
-                                Picture = new
-                                {
-                                    PictureId = p.User.Picture.PictureId,
-                                    ImageUrl = p.User.Picture.ImageUrl,
-                                }
-                            },
-                            Likes = p.Likes.Select(pl => new
-                            {
-                                UserId = pl.UserId,
-                                PostId = pl.PostId,
-                                IsLiked = pl.IsLiked,
-                                IsDisliked = pl.IsDisliked,
-                                User = new
-                                {
-                                    UserId = pl.User.UserId,
-                                    DisplayName = pl.User.DisplayName,
-                                }
-                            }).ToList(),
-                            Comments = p.Comments.Select(c => new
-                            {
-                                CommentId = c.CommentId,
-                                UserId = c.UserId,
-                                TimeStamp = c.TimeStamp,
-                                Content = c.Content,
-                                Likes = c.Likes.Select(cl => new
-                                {
-                                    IsLiked = cl.IsLiked,
-                                    IsDisliked = cl.IsDisliked,
-                                    User = new
-                                    {
-                                        UserId = cl.User.UserId,
-                                        DisplayName = cl.User.DisplayName,
-                                    }
-                                }).ToList(),
-                            }).ToList(),
-                        }).ToList()
                     }
                 }).ToListAsync();
 
-            var list = new List<dynamic>();
-            foreach (var item in friendFollowers)
+            List<int> ff = new List<int>();
+            foreach (var item in test)
             {
                 if (item.UserId != id)
                 {
-                    foreach (var post in item.User.Posts)
-                    {
-                        list.Add(post);
-                    }
+                    ff.Add(item.User.UserId);
+
                 }
                 else if (item.OtherUserId != id)
                 {
-                    foreach (var post in item.OtherUser.Posts)
-                    {
-                        list.Add(post);
-                    }
+                    ff.Add(item.OtherUser.UserId);
                 }
-            }
+            };
 
-            return Ok(list);
+            var posts = await context.Posts
+                .Where(g => ff.Contains(g.UserId))
+                .Select(p => new
+                {
+                    PostId = p.PostId,
+                    UserId = p.UserId,
+                    GroupId = p.GroupId,
+                    TimeStamp = p.TimeStamp,
+                    Content = p.Content,
+                    FriendOnly = p.FriendOnly,
+                    Latitude = p.Latitude,
+                    Longitude = p.Longitude,
+                    Group = new
+                    {
+                        GroupId = p.Group.GroupId,
+                        GroupName = p.Group.GroupName,
+                        Picture = new
+                        {
+                            PictureId = p.Group.Picture.PictureId,
+                            ImageUrl = p.Group.Picture.ImageUrl,
+                        }
+                    },
+                    User = new
+                    {
+                        UserId = p.User.UserId,
+                        DisplayName = p.User.DisplayName,
+                        Picture = new
+                        {
+                            PictureId = p.User.Picture.PictureId,
+                            ImageUrl = p.User.Picture.ImageUrl,
+                        }
+                    },
+                    Likes = p.Likes.Select(pl => new
+                    {
+                        UserId = pl.UserId,
+                        PostId = pl.PostId,
+                        IsLiked = pl.IsLiked,
+                        IsDisliked = pl.IsDisliked,
+                        User = new
+                        {
+                            UserId = pl.User.UserId,
+                            DisplayName = pl.User.DisplayName,
+                        }
+                    }).ToList(),
+                    Comments = p.Comments.Select(c => new
+                    {
+                        CommentId = c.CommentId,
+                    }).ToList(),
+                }).ToListAsync();
+
+            return Ok(posts);
         }
 
         [HttpGet("followerfeed/{id:int}")]
