60c043adc4e5a3fb6ea998ead9464ffcd6ef5884
Sebastian Munch <seba2068@elev.tec.dk>
2023-07-28 08:14:05 +0200

    Added Group Service

---
+++ TECHUB.Service/Interfaces/IGroupService.cs
@@ -0,0 +1,11 @@
+﻿using TECHUB.Repository.Models;
+
+namespace TECHUB.Service.Interfaces
+{
+    public interface IGroupService
+    {
+        Task<Group> AddGroup(Group group);
+        Task<Group> DeleteGroup(int Id);
+        Task<Group> UpdateGroup(Group group);
+    }
+}
