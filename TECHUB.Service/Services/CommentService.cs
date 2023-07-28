be82b5cdf41040240e9b47bda4a7ca3935db1c42
Sebastian Munch <seba2068@elev.tec.dk>
2023-07-28 13:04:09 +0200

    Comment service

--- TECHUB.Service/Services/CommentService.cs
+++ TECHUB.Service/Services/CommentService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TECHUB.Repository.Interfaces;
+﻿using TECHUB.Repository.Interfaces;
 using TECHUB.Repository.Models;
 using TECHUB.Service.Interfaces;
 
