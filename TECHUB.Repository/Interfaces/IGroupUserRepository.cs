d07e2b8cc41dd4d78f860a60f54bca216698e7c0
Sebastian Munch <seba2068@elev.tec.dk>
2023-07-31 08:46:01 +0200

    add GroupUserRepository

---
+++ TECHUB.Repository/Interfaces/IGroupUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using TECHUB.Repository.Models;
+
+namespace TECHUB.Repository.Interfaces
+{
+    public interface IGroupUserRepository
+    {
+        public Task<GroupUser> AddGroupInvitation(GroupUser groupUser);
+        public Task<GroupUser> DeleteGroupUser(int id);
+    }
+}
